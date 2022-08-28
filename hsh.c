a5184ecb771442f93385da7e802414a1862c0a23
xz <odufeso1@gmail.com>
2022-08-28 07:19:51 +0100

    Updated Shell to read input with getline()

--- hsh.c
+++ hsh.c
@@ -2,33 +2,53 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _arguments - allocates memory for arguments
  * @str: the argument string
- * @n: number of char in the string
- *
+ * @n: length of str (including NULL byte)
+ * @x: Number of words in the command
  * Return: pointer to argument string
  */
-char **_arguments(char *str, int n)
+char **_arguments(char *str, int n, int x)
 {
 	char **s;
-	int i;
+	int i, len;
 
-	s = malloc(2 * sizeof(char *));
+	s = malloc(x + 1 * sizeof(char *));
 	if (s == NULL)
 		return (NULL);
-	s[1] = NULL;
-	s[0] = malloc((n + 1) * sizeof(char));
-	if (s[0] == NULL)
+	s[x] = NULL;
+	for (i = 0; i < x; i++)
 	{
-		for (i = 0; i < 2; i++)
-			free(s[i]);
-		free(s);
-		return (NULL);
+		/**
+		 * This is where strtok() comes in. n now becomes
+		 * the length of the tokens returned from strk()
+		 */
+		s[0] = malloc((n) * sizeof(char));
+		if (s[0] == NULL)
+		{
+			for (i = 0; i <= x; i++)
+				free(s[i]);
+			free(s);
+			return (NULL);
+		}
 	}
-	for (i = 0; i < n; i++)
+	len = n - 1;
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] == ' ')
+		{
+			/**
+			  * This if statement might not be needed
+			  * when integrating strtok() into the function
+			  */
+			s[0][i] = '\0';
+			continue;
+		}
 		s[0][i] = str[i];
+	}
 	s[0][i] = '\0';
 
 	return (s);
@@ -46,40 +66,36 @@ char **_arguments(char *str, int n)
 int main(__attribute__((unused)) int argc, char **argv)
 {
 	char **av;
-	int i = 0, n = 1, x;
-	pid_t xxx;
-	char command[50], ch;
+	size_t size = 1;
+	char *command = NULL;
+	int x = 1, i;
+	pid_t fork_process;
 
-	while (n)
+	while (x)
 	{
-		for (i = 0; i < 50; i++)
-			command[i] = '\0';
 		printf("#cisfun$ ");
-		ch = getchar();
-		i = 0;
-		while (ch != '\n' && ch != EOF)
+		getline(&command, &size, stdin);
+		for (i = 0; command[i]; i++)
 		{
-			command[i] = ch;
-			i++;
-			ch = getchar();
+			if (command[i] == ' ' && command[i + 1] != ' ')
+				x++;
 		}
-		if (ch == EOF)
-			break;
-		av = _arguments(command, i);
-		xxx = fork();
-		if (xxx == -1)
+		av = _arguments(command, strlen(command), x);
+		free(command);
+		fork_process = fork();
+		if (fork_process == -1)
 			return (-1);
-		if (xxx == 0)
+		if (fork_process == 0)
 		{
-			if (execve(command, argv, NULL) == -1)
+			if (execve(av[0], av, NULL) == -1)
 				printf("%s: No such file or directory\n", argv[0]);
 			exit(0);
 		}
 		else
 		{
 			wait(NULL);
-			for (x = 0; x < 2; x++)
-				free(av[x]);
+			for (i = 0; i < x; i++)
+				free(av[i]);
 			free(av);
 		}
 	}
