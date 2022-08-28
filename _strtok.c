2a94cdf394d9ca57034445e468907e4a029d8fc8
xz <odufeso1@gmail.com>
2022-08-28 14:29:53 +0100

    Updates

--- _strtok.c
+++ _strtok.c
@@ -21,6 +21,13 @@ char *_strtok(char *str, const char *delim)
 		{
 			del = delim[delimCount++];
 			t = str[strCount] == del ? 1 : 0;
+			/**
+			 *if (t && str[strCount + 1] == del)
+			 *{
+			 *	t = 0;
+			 *	str[strCount] == '\0';
+			 *}
+			 */
 		}
 		if (t)
 		{
@@ -35,4 +42,19 @@ char *_strtok(char *str, const char *delim)
 		holder = NULL;
 
 	return (token);
+}
+
+int main()
+{
+	char str[] = "Itachi-Sasuke-Naruto";
+	char *delim = "-", *buf;
+
+	buf = _strtok(str, delim);
+	while (buf)
+	{
+		printf("%s\n", buf);
+		buf = _strtok(NULL, buf);
+	}
+
+	return (0);
 }
