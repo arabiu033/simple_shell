4d175be22c77bf3daf7912c2c6f9e7e8eea7e0d2
Aminu Rabiu <arabiu033@gmail.com>
2022-08-28 04:42:19 -0700

    added the _strtok.c function

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *_strtok(char *str, const char *delim);
+
+#endif /* #define _SHELL_H_ */
