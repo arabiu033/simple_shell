c0897306e202721a2ca5804ba36f082f435c7913
Aminu Rabiu <106544844+arabiu033@users.noreply.github.com>
2022-09-08 01:27:02 +0100

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -21,7 +21,7 @@ void _puts(char *str)
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to print.
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
